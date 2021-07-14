436ace234a39c4959426753939458e7c42cda8df
egnor <egnor@ofb.net>
2021-07-14 01:36:19 -0700

    blind code

--- tv_power.py
+++ tv_power.py
@@ -1,42 +1,14 @@
 #!/usr/bin/env python3
 
 import argparse
-import ctypes
+import cec
 import dbus
 import dbus.mainloop.glib
 import gi.repository.GLib
+import socket
 import sys
 import time
 
-
-libcec = ctypes.CDLL("libcec.so.6")
-
-def set_tv(state):
-    class cec_configuration(ctypes.Structure):
-        _fields_ = [
-            ("clientVersion", ctypes.c_uint32),
-            ("strDeviceName", ctypes.c_char * 15),
-            ("deviceTypes", ctypes.c_int * 5),
-            ("padding", ctypes.c_uint8 * 1024),
-        ]
-
-    class cec_adapter_descriptor(ctypes.Structure):
-        _fields_ = [
-        ]
-
-    config = cec_configuration()
-    libcec.libcec_clear_configuration(ctypes.byref(config))
-    config.strDeviceName = b"tv_power.py"
-    config.deviceTypes[0] = 4  # CEC_DEVICE_TYPE_PLAYBACK_DEVICE
-    libcec.libcec_initialise.restype = ctypes.c_void_p
-    conn = libcec.libcec_initialise(ctypes.byref(config))
-
-
-    print(type(conn))
-    # lib = cec.ICECAdapter.Create(config)
-    # adapters = lib.DetectAdapters(ctypes.c_char_p(), True)
-    # for adapter in adapters:
-    #     print(adapter.strComPath)
 
 
 class ScreenSaverListener:
@@ -71,6 +43,64 @@ class ScreenSaverListener:
         self._loop.run()
 
 
+class CecController:
+    def __init__(self, debug=False):
+        self._debug = debug
+
+        config = cec.libcec_configuration()
+        config.strDeviceName = socket.gethostname()
+        config.deviceTypes.Add(cec.CEC_DEVICE_TYPE_PLAYBACK_DEVICE)
+        config.setLogCallback(self._cec_log_callback)
+
+        self._adapter_open = None
+        self._lib = cec.ICECAdapter.Create(config)
+        if not self._lib:
+            raise IOError("ICECAdapter.Create failed")
+
+    def set_power(power_on):
+        if self._adapter_open:
+            try:
+                self._send_set_power(power_on)
+            except IOError as e:
+                print(f"*** {e} - retrying", file=sys.stderr)
+                self._adapter_open = None
+                lib.Close()
+
+        adapters = self._lib.DetectAdapters()
+        if not adapters:
+            raise FileNotFoundError("No CEC adapters found")
+        if not lib.Open(adapters[0].strComName):
+            raise IOError("CEC adapter Open failed")
+
+        self._adapter_open = adapters[0]
+        if self._debug:
+            name = self._adapter_open.strComName
+            print(f"=== Using CEC adapter: {name}", file=sys.stderr)
+
+        self._send_set_power(power_on)
+
+    def _send_set_power(power_on):
+        if power_on:
+            if self._debug:
+                print(f"=== Sending CEC PowerOnDevices", file=sys.stderr)
+            if not self._lib.PowerOnDevices(cec.CECDEVICE_TV):
+                raise IOError("CEC PowerOnDevices failed")
+        else:
+            if self._debug:
+                print(f"=== Sending CEC StandbyDevices", file=sys.stderr)
+            if not self._lib.StandbyDevices(cec.CECDEVICE_TV):
+                raise IOError("CEC StandbyDevices failed")
+
+    def _cec_log_callback(self, level, time, message):
+        prefix = {
+            getattr(cec, f"CEC_LOG_{level}"): level
+            for level in ["ERROR", "WARNING", "NOTICE", "TRAFFIC", "DEBUG"]
+        }
+
+        if self._debug or level >= cec.CEC_LOG_WARNING:
+            print(f"CEC {prefix.get(level, level)}: {message}", file=sys.stderr)
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--debug', action='store_true')
 
@@ -80,17 +110,21 @@ mode_group.add_argument('--on', action='store_true')
 mode_group.add_argument('--dbus', action='store_true')
 args = parser.parse_args()
 
+cec = CecController(debug=args.debug)
 if args.off:
-    set_tv(False)
+    cec.set_power(False)
 elif args.on:
-    set_tv(True)
+    cec.set_power(True)
 elif args.dbus:
     listener = ScreenSaverListener(debug=args.debug)
     last_on = time.time()
-    set_tv(True)
+    cec.set_power(True)
     while True:
         listener.wait_for_signal()
         now = time.time()
         if now > last_on + 10.0 and not listener.blanked:
-            set_tv(True)
-            last_on = now
+            try:
+                cec.set_power(True)
+                last_on = now
+            except IOError as e:
+                print(f"*** {e}", file=sys.stderr)
