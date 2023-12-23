c4fc655a4b997c6b1cf3ad2701bd08cf8c144f78
Dan Egnor <egnor@ofb.net>
2023-12-23 13:16:10 -0800

    refactor

--- install.py
+++ install.py
@@ -4,47 +4,57 @@ import os
 import os.path
 import stat
 
-def update_links(source_rel, home_rel):
-    normjoin = lambda *paths: os.path.normpath(os.path.join(*paths))
+def normjoin(*paths):
+    return os.path.normpath(os.path.join(*paths))
 
-    source_abs = normjoin(os.getcwd(), os.path.dirname(__file__), source_rel)
-    home_abs = normjoin(os.environ["HOME"], home_rel)
-    source_to_home = os.path.relpath(source_abs, start=home_abs)
-    links = {f: normjoin(source_to_home, f) for f in os.listdir(source_abs)}
+home_abs = os.environ["HOME"]
+source_abs = normjoin(os.getcwd(), os.path.dirname(__file__), "homedir")
+home_to_source = os.path.relpath(source_abs, start=home_abs)
 
-    os.makedirs(home_abs, exist_ok=True)
-    for existing_name in sorted(os.listdir(home_abs)):
-        existing_abs = normjoin(home_abs, existing_name)
+links = {}
+for source_sub, subdirs, names in os.walk(source_abs):
+    sub_rel = os.path.relpath(source_sub, start=source_abs)
+    home_sub = os.path.join(home_abs, sub_rel)
+    home_to_source_sub = os.path.relpath(source_sub, start=home_sub)
+    for name in names:
+        links[normjoin(sub_rel, name)] = normjoin(home_to_source_sub, name)
+
+os.makedirs(home_abs, exist_ok=True)
+for home_sub, subdirs, names in os.walk(home_abs):
+    sub_rel = os.path.relpath(home_sub, start=home_abs)
+    for name in names:
+        existing_abs = normjoin(home_sub, name)
         try:
             existing_link = os.readlink(existing_abs)
         except OSError:
             continue
 
-        tilde_rel = normjoin("~", home_rel, existing_name)
-        desired_link = links.pop(existing_name, None)
-        if not desired_link and "dotfiles" in existing_link:
+        name_rel = normjoin(sub_rel, name)
+        tilde_rel = normjoin("~", name_rel)
+        desired_link = links.pop(name_rel, None)
+        if desired_link == existing_link:
+            print(f"(keep {tilde_rel} => {existing_link})")
+        elif not desired_link and "dotfiles" in existing_link:
             print(f"*remove* {tilde_rel} (was => {existing_link})")
             os.remove(existing_abs)
-        elif not desired_link:
-            print(f"(ignore {tilde_rel} => {existing_link})")
-        elif desired_link == existing_link:
-            print(f"(keep {tilde_rel} => {existing_link})")
-        else:
+        elif desired_link:
             print(
                 f"*update* {tilde_rel} => {desired_link} "
                 f"(was {existing_link})")
             os.remove(existing_abs)
             os.symlink(desired_link, existing_abs)
 
-    for new_name, new_link in sorted(links.items()):
-        tilde_rel = normjoin("~", home_rel, new_name)
-        print(f"*link* {tilde_rel} => {new_link}")
-        target = normjoin(home_abs, new_name)
-        if os.path.isdir(target) and not os.listdir(target):
-            os.rmdir(normjoin(home_abs, new_name))
-        os.symlink(new_link, normjoin(home_abs, new_name))
+for new_name, new_link in sorted(links.items()):
+    tilde_rel = normjoin("~", new_name)
+    target = normjoin(home_abs, new_name)
+    if os.path.isdir(target) and not os.listdir(target):
+        print(f"*rmdir* {new_name}")
+        os.rmdir(normjoin(home_abs, new_name))
 
+    parent = os.path.dirname(target)
+    if not os.path.isdir(parent):
+        print(f"*mkdir* {parent}")
+        os.makedirs(parent, exist_ok=True)
 
-update_links("homedir", ".")
-update_links("dotconfig", ".config")
-update_links("dotssh", ".ssh")
+    print(f"*link* {tilde_rel} => {new_link}")
+    os.symlink(new_link, normjoin(home_abs, new_name))
