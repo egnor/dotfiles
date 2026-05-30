56e3a71367bdde77b1c242b9d0ec28e4919390b7
Dan Egnor <egnor@ofb.net>
2026-05-29 18:17:06 -0700

    source project-local .nvim.lua

--- user/files-modern/.config/nvim/lua/config/options.lua
+++ user/files-modern/.config/nvim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.colorcolumn = "81"
+vim.o.exrc = true -- source project-local .nvim.lua (with trust prompt)
 vim.opt.linebreak = false
 vim.opt.number = false
 vim.opt.relativenumber = false --
